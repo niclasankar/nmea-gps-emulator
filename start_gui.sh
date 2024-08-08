5ef676cfd9bcf8ec05a860e153ebeae174d5dfad
Niclas Ankar <49547948+niclasankar@users.noreply.github.com>
2024-08-08 17:00:57 +0200

    Edited start scripts

--- start_gui.sh
+++ start_gui.sh
@@ -4,7 +4,7 @@
 source venv/bin/activate
 
 # Run the Python script
-python ./src/nmea_gps_emulator/main.py
+python ./src/nmea_gps_emulator/main_gui.py
 
 # Deactivate the virtual environment (optional)
 #deactivate
